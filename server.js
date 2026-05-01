b05145143348d1015bcf05f21f5166f0d4eb77f5
Zikirullo <zikirulloabdumannopov@gmail.com>
2026-05-01 14:12:20 +0900

    feat: building simple backend server on nodejs

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/restj", (req, res) => {
+  res.json({ name: "Marco", age: 21, group: "MIT" });
+});
+
+app.listen(3000, () => {
+  console.log(`Backend server is running on port 3000`);
+});

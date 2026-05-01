023ec31136ff3c34c949826087003bfa941d2d53
Zikirullo <zikirulloabdumannopov@gmail.com>
2026-05-01 13:51:31 +0900

    feat: thinking about fullstack

---
+++ notice.js
@@ -0,0 +1,12 @@
+/*
+ Fullstack = Restaurant
+ 
+ Frontend development:
+    - BSSR(DTE/JTE/EJS)
+    - SPA (React/ Angular / Vue)
+
+    API request:
+     - Design    > Tranditional API/ Rest API/ GraphQL API
+     - Method    > GET/POST
+     - Structure > Header/Body
+ */
